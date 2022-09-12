a5f61c5925780628752df049cd9fff03b144cdea
Bradley May <mayb1991@yahoo.com>
2022-09-12 11:22:20 -0600

    added ncaaf & mlb odds still need to work on being able to follow games for users

--- app/routes.py
+++ app/routes.py
@@ -88,3 +88,71 @@ def scores():
             for d in data]
         return render_template('scores.html', game_data=game_data)
     return render_template('scores.html')
+
+
+@app.route("/ncaaf/odds", methods=["GET", "POST"])
+def ncaaf_odds():
+
+    url = "https://odds.p.rapidapi.com/v4/sports/americanfootball_ncaaf/odds"
+
+    querystring = {"regions": "us", "oddsFormat": "decimal",
+                   "markets": "spreads", "dateFormat": "iso"}
+
+    headers = {
+        "X-RapidAPI-Key": "4afe651181msh7783e50a831acc1p11a544jsnd7f3fdcc2478",
+        "X-RapidAPI-Host": "odds.p.rapidapi.com"
+    }
+
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring)
+
+    # print(response.text)
+
+    if response.ok:
+        data = response.json()
+
+        ncaaf = [{
+
+            # "id": data["id"],
+            # "Sport": data["sport_key"],
+            "Match": [d['home_team'], d['away_team']],
+            "Game_Time": d['commence_time'],
+            "Odds": d['bookmakers'][5]['markets'][0]['outcomes'][0]['point'],
+            "Favorite": d['bookmakers'][5]['markets'][0]['outcomes'][0]['name']
+        }
+        for d in data]
+        return render_template("ncaaf.html", ncaaf=ncaaf)
+
+
+@app.route("/mlb/odds", methods=["GET", "POST"])
+def mlb_odds():
+
+    url = "https://odds.p.rapidapi.com/v4/sports/baseball_mlb/odds"
+
+    querystring = {"regions": "us", "oddsFormat": "decimal",
+                   "markets": "spreads", "dateFormat": "iso"}
+
+    headers = {
+        "X-RapidAPI-Key": "4afe651181msh7783e50a831acc1p11a544jsnd7f3fdcc2478",
+        "X-RapidAPI-Host": "odds.p.rapidapi.com"
+    }
+
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring)
+
+    # print(response.text)
+
+    if response.ok:
+        data = response.json()
+
+        mlb = [{
+
+            # "id": data["id"],
+            # "Sport": data["sport_key"],
+            "Match": [d['home_team'], d['away_team']],
+            "Game_Time": d['commence_time'],
+            "Odds": d['bookmakers'][5]['markets'][0]['outcomes'][0]['point'],
+            "Favorite": d['bookmakers'][5]['markets'][0]['outcomes'][0]['name']
+        }
+        for d in data]
+        return render_template("mlb_odds.html", mlb=mlb)
